483292463089b365869641570261184f66963636
Henrik-Johannes Stromberg <stromberg@imw.tu-clausthal.de>
2019-11-24 08:42:16 +0100

    Further work

---
+++ IMWJQ-Server/IMWJQ/frmmain.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TaskWatcher = New System.IO.FileSystemWatcher()
+        Me.ClientWatcher = New System.IO.FileSystemWatcher()
+        CType(Me.TaskWatcher, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ClientWatcher, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TaskWatcher
+        '
+        Me.TaskWatcher.EnableRaisingEvents = True
+        Me.TaskWatcher.Filter = "*.csv*"
+        Me.TaskWatcher.NotifyFilter = System.IO.NotifyFilters.LastWrite
+        Me.TaskWatcher.SynchronizingObject = Me
+        '
+        'ClientWatcher
+        '
+        Me.ClientWatcher.EnableRaisingEvents = True
+        Me.ClientWatcher.SynchronizingObject = Me
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "frmmain"
+        Me.Text = "frmmain"
+        CType(Me.TaskWatcher, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ClientWatcher, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TaskWatcher As IO.FileSystemWatcher
+    Friend WithEvents ClientWatcher As IO.FileSystemWatcher
+End Class
---
+++ IMWJQ-Server/IMWJQ/frmmain.vb
@@ -0,0 +1,79 @@
+﻿Public Class frmmain
+    Private Clients As List(Of Client)
+    Private Jobs As List(Of Job)
+
+    Private Structure Job
+        Dim Software As String
+        Dim Ags As String
+        Dim Owner As String
+        Dim Timeout As Integer
+        Dim Client As List(Of String)
+        Dim Status As Byte
+    End Structure
+
+    Private Structure Client
+        Dim Name As String
+        Dim Software As List(Of String)
+    End Structure
+
+    Private Sub frmmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Clients = LoadClients()
+        Jobs = New List(Of Job)
+
+        Jobs = LoadJobs(Jobs)
+    End Sub
+
+    Private Function LoadClients() As List(Of Client)
+        Dim res As New List(Of Client)
+        Using MyReader As New Microsoft.VisualBasic.FileIO.
+        TextFieldParser(My.Application.Info.DirectoryPath & "\Clients.csv")
+            MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader.Delimiters = New String() {";"}
+            Dim currentRow As String()
+            MyReader.ReadFields()
+            While Not MyReader.EndOfData
+                Dim nclient As New Client
+                currentRow = MyReader.ReadFields()
+                nclient.Name = currentRow(0)
+                nclient.Software = New List(Of String)
+                Dim sw() As String
+                sw = Split(currentRow(1), ",")
+                Dim i As Integer
+                For i = 0 To UBound(sw)
+                    nclient.Software.Add(sw(i))
+                Next i
+                res.Add(nclient)
+            End While
+        End Using
+
+        Return res
+    End Function
+
+    Private Function LoadJobs(Path As String, res As List(Of Job)) As List(Of Job)
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(Path)
+            MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader.Delimiters = New String() {";"}
+            Dim currentRow As String()
+            MyReader.ReadFields()
+            While Not MyReader.EndOfData
+                Dim njob As New Job
+                currentRow = MyReader.ReadFields()
+                njob.Software = currentRow(0)
+                njob.Ags = currentRow(1)
+                njob.Owner = currentRow(2)
+                njob.Timeout = currentRow(3)
+                njob.Client = New List(Of String)
+                Dim splitClients() As String
+                splitClients = Split(currentRow(4), ",")
+                Dim i As Integer
+                For i = 0 To UBound(splitClients)
+                    njob.Client.Add(splitClients(i))
+                Next i
+                njob.Status = 0
+                res.Add(njob)
+            End While
+        End Using
+        Return res
+    End Function
+
+End Class
